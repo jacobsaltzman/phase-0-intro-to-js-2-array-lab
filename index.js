1c9860ebb357afd29ea25596ba25aa0876ea1725
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-05-02 18:00:53 -0700

    commit tree 03601224c19266504b3825fc30bac2e1709adb82

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(cat){
+    return cats.push(cat);
+}
+
+function destructivelyPrependCat(cat){
+    return cats.unshift(cat);
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(cat){
+    return [...cats, cat];
+}
+
+function prependCat(cat){
+    return [cat, ...cats];
+}
+
+function removeLastCat(){
+    const newCats = cats.slice(0, -1);
+    return newCats;
+}
+
+function removeFirstCat(){
+    const newCats = cats.slice(1)
+    return newCats;
+}
